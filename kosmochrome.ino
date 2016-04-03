4bce2bc618125f4c9fd8fa2537e0de25b3400ab2
jake waters <jerkey@g-cipher.net>
2016-04-02 23:51:13 -0700

    it works

---
+++ kosmochrome.ino
@@ -0,0 +1,26 @@
+#define FET_A A1
+#define FET_B 11
+#define FET_C 12
+
+void setup() {
+  Serial.begin(57600);
+  Serial.println('kosmochrome');
+  pinMode(FET_A,OUTPUT);
+  pinMode(FET_B,OUTPUT);
+  pinMode(FET_C,OUTPUT);
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    int inputValue = Serial.parseInt();
+    Serial.println(inputValue);
+    digitalWrite(FET_A,inputValue & 1);
+    digitalWrite(FET_B,inputValue & 2);
+    digitalWrite(FET_C,inputValue & 4);
+    delay(inputValue);
+    digitalWrite(FET_A,LOW);
+    digitalWrite(FET_B,LOW);
+    digitalWrite(FET_C,LOW);
+  }
+  digitalWrite(13,! digitalRead(13));
+}
