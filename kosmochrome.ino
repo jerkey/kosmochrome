7ca4d17fd9b0f214279045caab5aeb0129a07503
jerkey <jerkey@g-cipher.net>
2017-03-26 00:26:41 -0700

    use this HSV routine from the internet

--- kosmochrome.ino
+++ kosmochrome.ino
@@ -24,23 +24,73 @@ void loop() {
     int inputValue = Serial.parseInt();
     Serial.println(inputValue);
   }
-  rgb_fade(10);
+  rgb_fade(100);
   digitalWrite(13,! digitalRead(13));
 }
 
-void rgb_fade(int idelay) { //-FADE LEDS THROUGH HSV RAINBOW
-  ibright++;
-  if (ibright >= MAX_BRIGHT) {
-    ibright = MIN_BRIGHT;
-    idex--;
-    if (idex < 0) {
-      idex = 2;
-    }
+void rgb_fade(int idelay) { //The Hue value will vary from 0 to 360, which represents degrees in the color wheel
+  for(int hue=0;hue<360;hue++) {
+    setLedColorHSV(hue,1,1); //We are using Saturation and Value constant at 1
+    delay(idelay);
   }
-  thisColor[idex] = ibright;
-  thisColor[(idex + 1) % 3] = MAX_BRIGHT - ibright + MIN_BRIGHT;
-  one_color_all(thisColor[0],thisColor[1],thisColor[2]);
-  delay(idelay);
+}
+
+//Convert a given HSV (Hue Saturation Value) to RGB(Red Green Blue) and set the led to the color
+//  h is hue value, integer between 0 and 360
+//  s is saturation value, double between 0 and 1
+//  v is value, double between 0 and 1
+//http://splinter.com.au/blog/?p=29
+void setLedColorHSV(int h, double s, double v) { //this is the algorithm to convert from RGB to HSV
+  double r=0;
+  double g=0;
+  double b=0;
+
+  double hf=h/60.0;
+
+  int i=(int)floor(h/60.0);
+  double f = h/60.0 - i;
+  double pv = v * (1 - s);
+  double qv = v * (1 - s*f);
+  double tv = v * (1 - s * (1 - f));
+
+  switch (i) {
+  case 0: //rojo dominante
+    r = v;
+    g = tv;
+    b = pv;
+    break;
+  case 1: //verde
+    r = qv;
+    g = v;
+    b = pv;
+    break;
+  case 2:
+    r = pv;
+    g = v;
+    b = tv;
+    break;
+  case 3: //azul
+    r = pv;
+    g = qv;
+    b = v;
+    break;
+  case 4:
+    r = tv;
+    g = pv;
+    b = v;
+    break;
+  case 5: //rojo
+    r = v;
+    g = pv;
+    b = qv;
+    break;
+  }
+
+  int red=constrain((int)255*r,0,255);
+  int green=constrain((int)255*g,0,255);
+  int blue=constrain((int)255*b,0,255);
+
+  one_color_all(red,green,blue);
 }
 
 void one_color_all(int red, int grn, int blu) {
