2d0be782808800e3301380ab38d50562d7f2dc3f
jerkey <jerkey@g-cipher.net>
2017-03-25 22:18:03 -0700

    new hardware with PWM pins

--- kosmochrome.ino
+++ kosmochrome.ino
@@ -1,13 +1,13 @@
-#define FET_A A1
-#define FET_B 11
-#define FET_C 12
+#define RED 9
+#define GREEN 10
+#define BLUE 11
 
 void setup() {
   Serial.begin(57600);
   Serial.println('kosmochrome');
-  pinMode(FET_A,OUTPUT);
-  pinMode(FET_B,OUTPUT);
-  pinMode(FET_C,OUTPUT);
+  pinMode(RED,OUTPUT);
+  pinMode(GREEN,OUTPUT);
+  pinMode(BLUE,OUTPUT);
   Serial.setTimeout(50);
 }
 
@@ -15,13 +15,13 @@ void loop() {
   while (Serial.available() > 0) {
     int inputValue = Serial.parseInt();
     Serial.println(inputValue);
-    digitalWrite(FET_A,inputValue & 1);
-    digitalWrite(FET_B,inputValue & 2);
-    digitalWrite(FET_C,inputValue & 4);
+    digitalWrite(RED,inputValue & 1);
+    digitalWrite(GREEN,inputValue & 2);
+    digitalWrite(BLUE,inputValue & 4);
     delay(inputValue);
-    digitalWrite(FET_A,LOW);
-    digitalWrite(FET_B,LOW);
-    digitalWrite(FET_C,LOW);
+    digitalWrite(RED,LOW);
+    digitalWrite(GREEN,LOW);
+    digitalWrite(BLUE,LOW);
   }
   digitalWrite(13,! digitalRead(13));
 }
